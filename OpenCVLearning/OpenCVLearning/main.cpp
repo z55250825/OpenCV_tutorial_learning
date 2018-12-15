6de0fcf0d32b9f7553643324b1bc76e9ee2ab145
z55250825 <z55250825@163.com>
2018-12-15 22:36:10 +0800

    初始设置

---
+++ OpenCVLearning/OpenCVLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVLearning
+//
+//  Created by z55250825 on 2018/12/14.
+//  Copyright © 2018 z55250825. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
