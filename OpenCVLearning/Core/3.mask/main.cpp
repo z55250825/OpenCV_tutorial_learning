1556828fe5c044ced27cedb22718fc837ed6146c
z55250825 <z55250825@163.com>
2018-12-16 15:16:49 +0800

    mask operation

---
+++ OpenCVLearning/Core/3.mask/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mask
+//
+//  Created by z55250825 on 2018/12/16.
+//  Copyright © 2018 z55250825. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
