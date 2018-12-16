9ee10626c9ab6e1feee4f85609db61dd626e0b3b
z55250825 <z55250825@163.com>
2018-12-16 13:14:04 +0800

    pixel scan

---
+++ OpenCVLearning/Core/pixel/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  pixel
+//
+//  Created by z55250825 on 2018/12/16.
+//  Copyright © 2018 z55250825. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    return 0;
+}
